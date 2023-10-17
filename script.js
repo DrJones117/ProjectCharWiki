7f4024c730789d1c8208c2719a0e986b59bcfec6
Judah Kahler <judah.kahler@gmail.com>
2023-10-17 11:59:38 -0500

    Calm before the strom

---
+++ script.js
@@ -0,0 +1,10 @@
+
+function buttonChange(element) {
+    element.style.backgroundColor = "white";
+    element.style.color = "black";
+}
+
+function buttonRevert(element) { 
+    element.style.backgroundColor = "inherit";
+    element.style.color = "white";
+}
