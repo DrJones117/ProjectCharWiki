802426ccd8f2cf8ae663ed93a2e719fbfad2990f
Judah Kahler <judah.kahler@gmail.com>
2023-10-17 17:18:53 -0500

    Basic image expansion

--- script.js
+++ script.js
@@ -8,3 +8,25 @@ function buttonRevert(element) {
     element.style.color = "white";
 }
 
+// ======= Expanding Image ====== //
+
+function exImg(element) {
+    element.style.transition = "500ms";
+    element.style.height = "200px";
+    element.style.width = "200px";
+    element.style.position = "relative";
+}
+
+function decrMain(element) {
+    element.style.transition = "500ms";
+    element.style.height = "130px";
+    element.style.width = "130px";
+    element.style.position = "static";
+}
+
+function decrSide(element) {
+    element.style.transition = "500ms";
+    element.style.height = "100px";
+    element.style.width = "100px";
+    element.style.position = "static";
+}
